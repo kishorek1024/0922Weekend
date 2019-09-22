f2c996ba1583e44b5cd987b3c5febb6a50a681ff
kishore.k1024@gmail.com <kishore.k1024@gmail.com>
2019-09-22 10:57:37 +0530

    Release1

---
+++ WebDriverLatest/src/packone/First.java
@@ -0,0 +1,31 @@
+package packone;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class First {
+
+	public static void main(String[] args) {
+		
+		
+		/*System.setProperty("webdriver.gecko.driver", "C:\\Users\\DELL\\Desktop\\WD99\\Jars\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://facebook.com");
+		driver.manage().window().maximize();*/
+		
+		/*System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\WD99\\Jars\\chromedriver.exe");
+		WebDriver driver1=new ChromeDriver();
+		driver1.get("http://rediff.com");
+		driver1.manage().window().maximize();*/
+		
+		System.setProperty("webdriver.ie.driver", "C:\\Users\\DELL\\Desktop\\WD99\\Jars\\IEDriverServer.exe");
+		WebDriver driver2=new InternetExplorerDriver();
+		driver2.get("http://gmail.com");
+		driver2.manage().window().maximize();
+		
+
+	}
+
+}
